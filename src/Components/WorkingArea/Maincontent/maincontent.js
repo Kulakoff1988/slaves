f2fdd952c0e59aff683dcac9a37bc4bf9c32397b
Alexander Kulakov <kulakofft4@gmail.com>
2019-03-15 11:16:16 +0300

    Some attempt to make working buttons

--- src/Components/WorkingArea/Maincontent/maincontent.js
+++ src/Components/WorkingArea/Maincontent/maincontent.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './maincontent.css'
+import './maincontent.css';
+import Kinds from '../Navigator/kindOfLife';
 
 class Maincontent extends React.Component {
+    // props = {
+    //     Humans:
+    //     Elfs:
+    //     Orcs:
+    //     Trolls:
+    //     Gnomes:
+    //     Ogres:
+    //     Protos:
+    // }
     render() {
         return (
-            <div className="maincontent">
-            </div>
+            <div className="maincontent"></div>
         )
     }
 };
